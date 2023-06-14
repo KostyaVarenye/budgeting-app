19ac6f40481e780f55c132f952c358f793f6ed8e
Kostya.Varennya <vekostya@gmail.com>
2023-06-14 14:39:29 +0300

    added tests to render the first, second, third components on page and added comments to outline .css

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders First', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/First/i);
   expect(linkElement).toBeInTheDocument();
+});
+test('renders Second', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Second/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Third', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Third/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders App component', () => {
+  render(<App />);
+  const linkElement = screen.queryByText(/Fourth/i);
+  expect(linkElement).not.toBeInTheDocument();
 });
