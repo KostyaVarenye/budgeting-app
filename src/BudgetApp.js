64867479fef3a741d693b5c50688bbcabc7bbd22
Kostya.Varennya <vekostya@gmail.com>
2023-06-15 13:47:33 +0300

    added d3 package dependency for graph drawings and Barchart to the graph component

--- src/BudgetApp.js
+++ src/BudgetApp.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './BudgetApp.css';
+import React from "react";
+import "./BudgetApp.css";
+import Barchart from "./components/BarChart";
+
+const myData = [
+  { category: 'Shopping', budget: 300, expenditure: 180 },
+  { category: 'Utilities', budget: 100, expenditure: 75 },
+  { category: 'Rent', budget: 500, expenditure: 500 },
+  { category: 'Savings', budget: 200, expenditure: 200 },
+  { category: 'Transportation', budget: 500, expenditure: 175 },
+];
 
 const BudgetApp = () => {
   return (
     <div className="budget-app">
-      <nav className="navbar">
-        {/* Navigation bar content */}
-      </nav>
+      <nav className="navbar">{/* Navigation bar content */}</nav>
       <div className="container">
         <div className="row">
           <div className="col-lg-4">
@@ -25,7 +32,7 @@ const BudgetApp = () => {
         <div className="row">
           <div className="col-lg-12">
             <h1>Graph</h1>
-            {/* Graph component */}
+            <Barchart data={myData}/>
           </div>
         </div>
       </div>
---
+++ src/components/BarChart.js
@@ -0,0 +1,62 @@
+import * as d3 from "d3";
+import { useEffect, useRef } from "react";
+const Barchart = ({ data }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const margin = { top: 30, right: 30, bottom: 70, left: 60 },
+      width = 800 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+    const svg = d3
+      .select(ref.current)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const x = d3
+      .scaleBand()
+      .range([0, width])
+      .domain(data.map((d) => d.category))
+      .padding(0.2);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+    const y = d3.scaleLinear().domain([0, 600]).range([height, 0]);
+    svg.append("g").call(d3.axisLeft(y));
+
+    // Budget bars
+    svg
+      .selectAll("budgetBar")
+      .data(data)
+      .join("rect")
+      .attr("x", (d) => x(d.category))
+      .attr("y", (d) => y(d.budget))
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => height - y(d.budget))
+      .attr("fill", "green");
+
+    // Expenditure bars
+    svg
+      .selectAll("expenditureBar")
+      .data(data)
+      .join("rect")
+      .attr("x", (d) => x(d.category))
+      .attr("y", (d) => y(d.budget)) // start red bar where green bar starts
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => height - y(d.expenditure)) // height is just the expenditure
+      .attr("fill", "red");
+  }, [data]);
+
+  return <svg width={800} height={400} ref={ref} />;
+};
+
+export default Barchart;
